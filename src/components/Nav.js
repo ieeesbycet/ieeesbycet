4b6966dd8e1377d41e77d595df7e5b1a6ef4c696
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-09-29 18:15:48 +0530

    first commit
    Fix build errors

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <h1>IEEE SB YCET</h1>
+      </nav>
+    );
+  }
+}
